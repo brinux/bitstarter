9b7514b2da31675fabbc67457a736245fead91d3
Fabrizio Zanette <fabrizio.zanette@gmail.com>
2013-07-20 04:07:58 +0200

    Versione di prova della pagina personalizzata

--- web.js
+++ web.js
@@ -3,6 +3,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+/*app.get(/\/public\/(.*)$/, function(request, response) {
+  	response.send(fs.readFileSync("public/" + request.params[0]).toString());
+});*/
+
+app.configure(function(){
+	app.use('/public', express.static(__dirname + '/public'));
+});
+
 app.get('/', function(request, response) {
   	response.send(fs.readFileSync("index.html").toString());
 });
